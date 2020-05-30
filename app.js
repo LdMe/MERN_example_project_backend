5569e23e54a95bf78154dad726c2303b86f5b040
LdMe <dlafuente003@gmail.com>
2020-05-30 11:35:26 +0200

    app.js entrance file created

---
+++ app.js
@@ -0,0 +1,7 @@
+c/*
+*				EXAMPLE BACKEND APP MADE WITH NODE
+*    app.js:
+*		- First we initialize the configration and then we run the app 
+*/
+const config = require('./Config/config');
+const app = config.initApp();
